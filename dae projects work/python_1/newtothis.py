9cb711bebcb48e9fccfaff155b7cad857c2e8761
Sound2170 <gamerchriatian17@gmail.com>
2024-11-06 18:12:30 -0500

    Uploading my updated algorithm and flow chart

---
+++ dae projects work/python_1/newtothis.py
@@ -0,0 +1,17 @@
+#snake case
+first_name = "boo boo gaga" #this is a string
+
+# camo case
+lastName = "boo boo ga ga" #
+nickName = "christian" #this is string brode
+
+age = 879509 #my real age totally
+bankAccountbalance = 2.49 #intiger
+
+print(first_name)
+print(bankAccountbalance)
+
+if bankAccountbalance > 100:
+    print("you're are still broke")
+elif bankAccountbalance < 100:
+    print("god get yo money up!")
---
+++ dae projects work/python_1/test1.py
@@ -0,0 +1,5 @@
+print("welcome to apple sauce")
+
+numberofUserAppleSuace = input("how many apple suaces do you want??")
+
+print("The user said" , numberofUserAppleSuace)
