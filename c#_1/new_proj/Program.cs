2eed8a3d2a975ca6b58addb5ebd29fcc8274d928
Sound2170 <gamerchriatian17@gmail.com>
2025-01-15 18:04:12 -0500

    made some progress on c#

---
+++ c#_1/new_proj/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+int myAge = 98;
+float averageScore = 21.1f;
+string favoriteColor = "Blue";
+favoriteColor = "green";
+//bool isSunny = false;
+myAge = 129;
+Console.WriteLine(favoriteColor);
+//Console.WriteLine(isSunny);
+Console.WriteLine(averageScore);
+Console.WriteLine(myAge);
+// variables ^^
+bool isSunny = true;
+
+if (isSunny)
+{
+    Console.WriteLine("let's have some ice cream!");
+}
+bool isRaining = false;
+bool isSnowing = true;
+
+if (isRaining)
+{
+    Console.WriteLine("Stay inside brode, it's raining out!!");
+}
+else if (isSnowing)
+{
+    Console.WriteLine("it's snowing!! go outside to have fun or you could stay inside to drink some hot hot chocalate");
+}
+
+int temperture = 67;
+
+if (temperture > 50)
+{
+    Console.WriteLine("it's warm outside! go out now!!");
+}
+else
+{
+    Console.WriteLine("it's not warm! you have been warned!");
+}
