f661b5e984408a4b29b3c185f64b54dce74f3779
Sound2170 <gamerchriatian17@gmail.com>
2025-02-19 18:15:14 -0500

    made tiny progress to unity scripting but im still trying to learn how to code

---
+++ unity_scripting_1/testing and learning.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class testingandlearning : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
